dfe54e2685bc2d818f7fd1d6d35429bd150a5e6e
vladshein <vlshein1@gmail.com>
2025-04-05 12:28:35 +0300

    add solutions to 4 tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,55 @@
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random();
+    }
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+        this.key = key;
+    }
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[];
+        
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+        }
+    }
+
+    abstract openDoor(key: Key);
+}
+
+class MyHouse extends House {
+    constructor(key: Key) {
+        super();
+        this.key = key;
+    }
+
+    openDoor(key: Key) {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
+
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
